aa97bb4d6e3db563cf0fdbf818205f1240f2cc6e
Jose Basurto <jgbasu@gmail.com>
2016-09-14 21:52:20 -0500

    Se implementa formato en cuadro de chat, mensajes de usuario a la izquierda de la ventana, los generados por el sistema a la derecha, para diferenciarlos en el texto plano se agrega una literal (U-usuario, S-sistema).
    Se permite cambiar el tamaño de la fuente en el cuadro de chat.

--- LenguajeNatural/src/lenguajenatural/Chat.java
+++ LenguajeNatural/src/lenguajenatural/Chat.java
@@ -11,6 +11,10 @@ import java.util.Calendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyledDocument;
 
 /**
  *
@@ -21,12 +25,15 @@ public class Chat extends javax.swing.JFrame {
     /**
      * Creates new form Chat
      */
+    private int fontSize;
+    
     public Chat() {
         initComponents();
         jTextArea2.setRows(2);
         jTextArea2.setColumns(10);
         jTextArea2.setLineWrap(true);
         jTextArea2.setWrapStyleWord(true);
+        fontSize = 13;
     }
 
     /**
@@ -38,8 +45,6 @@ public class Chat extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jScrollPane1 = new javax.swing.JScrollPane();
-        txtChat = new javax.swing.JTextArea();
         btnEnviar = new javax.swing.JButton();
         btnGuardar = new javax.swing.JButton();
         btnVolver = new javax.swing.JButton();
@@ -48,13 +53,12 @@ public class Chat extends javax.swing.JFrame {
         sldVolumen = new javax.swing.JSlider();
         lblVolumen = new javax.swing.JLabel();
         btnBorrar = new javax.swing.JButton();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTextPane1 = new javax.swing.JTextPane();
+        jSpinner1 = new javax.swing.JSpinner();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        txtChat.setEditable(false);
-        txtChat.setColumns(20);
-        txtChat.setRows(5);
-        jScrollPane1.setViewportView(txtChat);
 
         btnEnviar.setText("Enviar");
         btnEnviar.addActionListener(new java.awt.event.ActionListener() {
@@ -98,6 +102,18 @@ public class Chat extends javax.swing.JFrame {
             }
         });
 
+        jTextPane1.setEditable(false);
+        jScrollPane3.setViewportView(jTextPane1);
+
+        jSpinner1.setModel(new javax.swing.SpinnerNumberModel(13, 5, 20, 1));
+        jSpinner1.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                jSpinner1StateChanged(evt);
+            }
+        });
+
+        jLabel1.setText("Tamaño de fuente");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -108,10 +124,11 @@ public class Chat extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 304, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btnEnviar))
-                    .addComponent(jScrollPane1))
+                        .addComponent(btnEnviar)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addComponent(jScrollPane3))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                         .addComponent(btnVolver, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(btnGuardar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
@@ -119,32 +136,42 @@ public class Chat extends javax.swing.JFrame {
                         .addComponent(sldVolumen, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(lblVolumen))
-                    .addComponent(btnBorrar))
-                .addGap(21, 21, 21))
+                    .addComponent(btnBorrar)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jSpinner1, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(3, 3, 3)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 234, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(btnEnviar)
-                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addContainerGap(22, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(btnGuardar)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(btnVolver)
-                        .addGap(53, 53, 53)
+                        .addGap(18, 18, 18)
                         .addComponent(btnBorrar)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(94, 94, 94)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jSpinner1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 234, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(btnEnviar)
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 10, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(lblVolumen, javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(sldVolumen, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(106, 106, 106))))
+                            .addComponent(sldVolumen, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap())
         );
 
         pack();
@@ -155,7 +182,11 @@ public class Chat extends javax.swing.JFrame {
         int key = evt.getKeyCode();
                 
         if(key == java.awt.event.KeyEvent.VK_ENTER)
-            this.addString();           
+            try {
+                this.addString();
+        } catch (BadLocationException ex) {
+            Logger.getLogger(Chat.class.getName()).log(Level.SEVERE, null, ex);
+        }           
     }//GEN-LAST:event_jTextArea2KeyPressed
 
     private void jTextArea2KeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextArea2KeyReleased
@@ -166,20 +197,74 @@ public class Chat extends javax.swing.JFrame {
              jTextArea2.setText(null);
     }//GEN-LAST:event_jTextArea2KeyReleased
 
-    private void addString() {
+    private void addString() throws BadLocationException {
+        int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        int min = Calendar.getInstance().get(Calendar.MINUTE);
+        int sec = Calendar.getInstance().get(Calendar.SECOND);
+        String entrada;
+        
+        
+        /*txtChat.append(
+                    "[" + hour + ":" + min + ":" + sec + "] "
+                            +jTextArea2.getText() + "\n");*/
+        entrada = "[U" + hour + ":" + min + ":" + sec + "] "
+                            +jTextArea2.getText() + "\n";
+        //jTextPane1.
+        /**/
+        StyledDocument doc = jTextPane1.getStyledDocument();
+        int length = doc.getLength();
+            
+        SimpleAttributeSet attrs = new SimpleAttributeSet();
+        
+        StyleConstants.setBold(attrs, true);
+        StyleConstants.setAlignment(attrs, StyleConstants.ALIGN_LEFT);
+        StyleConstants.setFontSize(attrs, fontSize);
+        
+        
+        //StyleConstants.setFontSize(attrs, sec); //cambia tamño de fuente
+        doc.insertString(doc.getLength(), entrada, null);
+        doc.setParagraphAttributes(length+1, 1, attrs, false);
+        
+        
+        
+        
+        
+        this.GeneraRespuesta();
+        /*jTextPane1.getStyledDocument().insertString(jTextPane1.getStyledDocument().getLength(), entrada, attrs);
+        jTextPane1.getStyledDocument().setParagraphAttributes(jTextPane1.getStyledDocument().getLength(), 0, attrs, false);
+        
+        jTextPane1.getStyledDocument().insertString(jTextPane1.getStyledDocument().getLength(), respuesta, attrsResp);
+        jTextPane1.getStyledDocument().setParagraphAttributes(jTextPane1.getStyledDocument().getLength(), 0, attrsResp, false);*/
+        /**/
+        jTextArea2.setText(null);
+    }
+    
+    private void GeneraRespuesta() throws BadLocationException {
         int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
         int min = Calendar.getInstance().get(Calendar.MINUTE);
         int sec = Calendar.getInstance().get(Calendar.SECOND);
         
-        txtChat.append(
-                    "[" + hour + ":" + min + ":" + sec + "] "
-                            +jTextArea2.getText() + "\n"); 
-        jTextArea2.setText(null);
+        String respuesta;
+        respuesta = "[S" + hour + ":" + min + ":" + sec + "] "
+                            + "Respuesta generada por el sistema (temp)" + "\n";
+        
+        StyledDocument doc = jTextPane1.getStyledDocument();
+        int length = doc.getLength();
+        SimpleAttributeSet attrsResp = new SimpleAttributeSet();
+        StyleConstants.setBold(attrsResp, false);
+        StyleConstants.setAlignment(attrsResp, StyleConstants.ALIGN_RIGHT);
+        StyleConstants.setFontSize(attrsResp, fontSize);
+        doc.insertString(doc.getLength(), respuesta, null);
+        doc.setParagraphAttributes(length+1, 1, attrsResp, false);
     }
     
     private void btnEnviarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEnviarActionPerformed
-        // TODO add your handling code here:
-        this.addString();
+        try {
+            // TODO add your handling code here:
+            this.addString();
+        } catch (BadLocationException ex) {
+            Logger.getLogger(Chat.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
     }//GEN-LAST:event_btnEnviarActionPerformed
 
@@ -190,7 +275,8 @@ public class Chat extends javax.swing.JFrame {
             java.io.File file = fileChooser.getSelectedFile();
             try {
                 try (java.io.FileWriter fileWriter = new java.io.FileWriter(file)) {
-                    fileWriter.write(txtChat.getText());
+                    //fileWriter.write(txtChat.getText());
+                    fileWriter.write(jTextPane1.getText());
                 }
             } catch (IOException ex) {
                 Logger.getLogger(Chat.class.getName()).log(Level.SEVERE, null, ex);
@@ -200,8 +286,10 @@ public class Chat extends javax.swing.JFrame {
 
     private void btnVolverActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVolverActionPerformed
         Principal p = new Principal();
-        this.hide();
-        p.show();
+        //this.hide(); <- método obsoleto :P
+        this.setVisible(false);
+        //p.show(); <- método obsoleto :P
+        p.setVisible(true);
         
     }//GEN-LAST:event_btnVolverActionPerformed
 
@@ -213,10 +301,25 @@ public class Chat extends javax.swing.JFrame {
         int respuesta = JOptionPane.showConfirmDialog(null, mensaje, titulo, JOptionPane.YES_NO_OPTION);
         if (respuesta == JOptionPane.YES_OPTION)
         {
-            txtChat.setText("");
+            //txtChat.setText("");
+            jTextPane1.setText("");
         }
         
     }//GEN-LAST:event_btnBorrarActionPerformed
+
+    private void jSpinner1StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jSpinner1StateChanged
+        // TODO add your handling code here:
+        fontSize = (int)jSpinner1.getValue();
+        StyledDocument doc = jTextPane1.getStyledDocument();
+        //int length = doc.getLength();
+        SimpleAttributeSet attrs = new SimpleAttributeSet();
+        int length = doc.getLength();
+        StyleConstants.setFontSize(attrs, fontSize);
+        //StyleConstants.setBold(attrsResp, false);
+        //StyleConstants.setAlignment(attrsResp, StyleConstants.ALIGN_RIGHT);
+        //doc.insertString(doc.getLength(), respuesta, null);
+        doc.setParagraphAttributes(0, length, attrs, false);
+    }//GEN-LAST:event_jSpinner1StateChanged
 
     /**
      * @param args the command line arguments
@@ -258,11 +361,13 @@ public class Chat extends javax.swing.JFrame {
     private javax.swing.JButton btnEnviar;
     private javax.swing.JButton btnGuardar;
     private javax.swing.JButton btnVolver;
-    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JSpinner jSpinner1;
     private javax.swing.JTextArea jTextArea2;
+    private javax.swing.JTextPane jTextPane1;
     private javax.swing.JLabel lblVolumen;
     private javax.swing.JSlider sldVolumen;
-    private javax.swing.JTextArea txtChat;
     // End of variables declaration//GEN-END:variables
 }
